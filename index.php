7f4575009bcc24827a923d6dd02c08a91fac4af1
codeatt <sergiojunior2000@hotmail.com>
2018-11-30 18:40:46 -0200

    inserção de total

--- index.php
+++ index.php
@@ -3,27 +3,18 @@
   <head>
     <meta charset="utf-8">
     <title>Protipo Software de Evento</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   </head>
-  <style media="screen">
-    input[type="number"] {
-  width: 50px;
-}
-label {
-  font-weight: bold;
-  margin-left: 10px;
-}
-div.item {
-  border: 1px solid black;
-  padding: 10px;
-  margin: 5px;
-}
-  </style>
 
   <body>
-    <form action="cadastro.php" method="post" id="formulario">
-  <input type="button" id="novoProd" value="Novo serviço" />
+    <!-- Aqui segue o exemplo 1 -->
+    <form style="background-color: gray;" action="cadastro.php" method="post" id="formulario">
+  <input  type="button" id="novoProd" value="Novo serviço" />
   <input type="submit" value="Enviar" />
+  Valor total do evento é de R$
+  <p style="display:inline; color:yellow"id="result"></p>
+  <!-- <input type="text" id="result" size="20"> -->
   <div id="item" class="item">
     <label>Selecione o(s) serviço(s) para o seu evento:</label>
     <select name="produtoId[]">
@@ -31,27 +22,61 @@ div.item {
       <option value="5">Decoração 1</option>
       <option value="9">Transporte 1</option>
     </select>
-    <label>Quantidade(s):</label>
-    <input type="number" name="quant[]" />
+    <!-- <label>Quantidade(s):</label> -->
+    <!-- <input type="number" name="quant[]" /> -->
+    <select id="no" onchange="myFunction()">
+    <option>0</option>
+    <option>1</option>
+    <option>2</option>
+    <option>3</option>
+    <option>4</option>
+    <option>5</option>
+    <option>6</option>
+    <option>7</option>
+    <option>8</option>
+    <option>9</option>
+</select>
   </div>
 </form>
+<!-- Script Total R$ Evento -->
+<script>
+function myFunction() {
+    var no = document.getElementById("no");
+    var option = no.options[no.selectedIndex].text;
+    var txt = document.getElementById("result").value;
+    txt = option;
+    document.getElementById("result").innerHTML = txt*2;
+}
+</script>
+<!-- Button Exemplo 1 -->
+    <!-- <div id="carr">
+        <p style="display:inline">Serviço de Buffet 1<br> Iten(s):<input type="button" name="ser" onclick="add()" value="Adicionar ao Carrinho"><p style="display:inline" id="num">0</p></p>
+    </div> -->
   </body>
+<!-- Script Exemplo 1 -->
   <script type="text/javascript">
     $(document).ready(function() {
       $("#novoProd").click(function() {
-      var novoItem = $("#item").clone().removeAttr('id'); // para não ter id duplicado
+      var novoItem = $("#item").clone().removeAttr('id');
+       // para não ter id duplicado (Adicionar ID diferente na proxima atualizacao)
       novoItem.children('input').val(''); //limpa o campo quantidade
       $("#formulario").append(novoItem);
     });
   });
   </script>
+<!-- Script Exemplo 2 -->
+  <!-- <script &lt;script type="text/javascript">
+  function add() {
+    // OBS: Só considera o primeiro input
+    var numero = document.getElementById("num");
+    numero.innerHTML = parseInt(numero.innerHTML) + 1;
+  }
+</script> -->
+<!--
+<script>
+document.getElementById("result").innerHTML =
+"The value of is " + z + ".";
+</script> -->
 
-  <!-- No PHP você faz um foreach no POST[produtoId] para fazer o insert na base
-
-foreach ($_POST['produtoId'] as $key=> $value){
-    echo $value; // Id do produto
-    echo $_POST['quant'][$key]; // Quantidade
-}
-
-Isto evitaria as manipulações nos id's dos input's, reduzindo seu código jQuery e de certa forma simplificando o insert com PHP. -->
+<!-- Isto evitaria as manipulações nos id's dos input's, reduzindo seu código jQuery e de certa forma simplificando o insert com PHP. -->
 </html>
