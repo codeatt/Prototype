403cd7887ded38d09b8bb8f23045b57fa7dae762
codeatt <sergiojunior2000@hotmail.com>
2018-12-03 04:00:15 -0200

    copia de valores em classes

--- index.php
+++ index.php
@@ -8,39 +8,49 @@
   </head>
 
   <body>
-    <!-- Aqui segue o exemplo 1 -->
-    <form style="background-color: gray;" action="cadastro.php" method="post" id="formulario">
-  <input  type="button" id="novoProd" value="Novo serviço" />
-  <input type="submit" value="Enviar" />
+  <form style="background-color: gray;" action="" method="post" id="formulario">
+  <input type="button" id="novoProd" value="Novo serviço"/>
+  <input type="submit" value="Enviar" onclick=""/>
   Valor total do evento é de R$
-  <p style="display:inline; color:yellow"id="result"></p>
-  <!-- <input type="text" id="result" size="20"> -->
+  <p style="display:inline; color:yellow" id="result"></p>
   <div id="item" class="item">
+
+    <!-- Serviços -->
     <label>Selecione o(s) serviço(s) para o seu evento:</label>
     <select name="produtoId[]">
-      <option value="1">Buffet 1</option>
-      <option value="5">Decoração 1</option>
-      <option value="9">Transporte 1</option>
+      <option value="Buffet1">Buffet 1</option>
+      <option value="Decoração1">Decoração 1</option>
+      <option value="Transporte1">Transporte 1</option>
     </select>
-    <!-- <label>Quantidade(s):</label> -->
-    <!-- <input type="number" name="quant[]" /> -->
-    <select id="no" onchange="myFunction()">
-    <option>0</option>
-    <option>1</option>
-    <option>2</option>
-    <option>3</option>
-    <option>4</option>
-    <option>5</option>
-    <option>6</option>
-    <option>7</option>
-    <option>8</option>
-    <option>9</option>
-</select>
-  </div>
+
+    <!-- TOTAL de custos -->
+    <input type="number" class="qtd" onchange="myTotal()">
+
+    <!-- TOTAL de cada LINHA -->
+    <p style="display:inline">R$ </p><p style="display:inline" class='til'></p>
+   </div>
+
 </form>
-<!-- Script Total R$ Evento -->
+<!-- Função Calcula o TOTAL -->
 <script>
-function myFunction() {
+function myTotal() {
+	  var total = 0;
+    var total2 = 0;
+
+    var x = document.getElementsByClassName("qtd");
+    var i;
+
+    for (i = 0; i < x.length; i++) {
+    	total = total + parseInt(x[i].value);
+      total2 = parseInt(x[i].value);
+        document.getElementById("result").innerHTML = total;
+        document.getElementsByClassName("til")[i].innerHTML = total2;
+    }
+}
+</script>
+<!-- Função OFF -->
+<script>
+function myFunction2() {
     var no = document.getElementById("no");
     var option = no.options[no.selectedIndex].text;
     var txt = document.getElementById("result").value;
@@ -48,35 +58,50 @@ function myFunction() {
     document.getElementById("result").innerHTML = txt*2;
 }
 </script>
-<!-- Button Exemplo 1 -->
-    <!-- <div id="carr">
-        <p style="display:inline">Serviço de Buffet 1<br> Iten(s):<input type="button" name="ser" onclick="add()" value="Adicionar ao Carrinho"><p style="display:inline" id="num">0</p></p>
-    </div> -->
-  </body>
-<!-- Script Exemplo 1 -->
-  <script type="text/javascript">
+<!-- Função CLONA linha -->
+<script type="text/javascript">
     $(document).ready(function() {
       $("#novoProd").click(function() {
-      var novoItem = $("#item").clone().removeAttr('id');
-       // para não ter id duplicado (Adicionar ID diferente na proxima atualizacao)
-      novoItem.children('input').val(''); //limpa o campo quantidade
+      var novoItem = $("#item").clone().removeAttr('id'); // Deleta ID caso tenha
+      // document.getElementsByClassName("til").val("");
+      // $('input').removeAttr('value');
+      novoItem.children('til').val(''); //limpa o campo quantidade
       $("#formulario").append(novoItem);
     });
   });
   </script>
-<!-- Script Exemplo 2 -->
-  <!-- <script &lt;script type="text/javascript">
-  function add() {
-    // OBS: Só considera o primeiro input
-    var numero = document.getElementById("num");
-    numero.innerHTML = parseInt(numero.innerHTML) + 1;
-  }
-</script> -->
-<!--
-<script>
-document.getElementById("result").innerHTML =
-"The value of is " + z + ".";
-</script> -->
-
-<!-- Isto evitaria as manipulações nos id's dos input's, reduzindo seu código jQuery e de certa forma simplificando o insert com PHP. -->
+<!-- Função OFF - Emissão de Report -->
+  <script>
+function myReport() {
+	var myJSON = '{ "name":"John", "age":31, "city":"New York" }';
+	var myObj = JSON.parse(myJSON);
+    var x = document.getElementsByClassName("json");
+    var i;
+    for (i = 0; i < x.length; i++) {
+        x[i].innerHTML = myObj.age;
+    }
+}
+</script>
+  </body>
 </html>
+
+<?php
+// $figura = $_POST['figura'];
+//
+// switch ($figura) {
+//     case "Buffet1":
+//         echo ($_POST['valor']*$_POST['valor2'];
+//         break;
+//     case "Decoração1":
+//         echo ($_POST['valor']*$_POST['valor2'];
+//         break;
+//     case "Decoração1":
+//         echo ($_POST['valor']*$_POST['valor2'];
+//         break;
+//    case "quatro":
+//         echo ($_POST['valor']*$_POST['valor2'];
+//         break;
+//     default:
+//         //nada
+// }
+?>
